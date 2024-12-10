a65906df37802816b51edb1303b11f8c80aeffc0
Mark J. Walls <48637841+mawa2559@users.noreply.github.com>
2024-12-10 15:08:48 -0700

    Update AddSFtoOktaGroup.ps1

--- OktaScripts/AddSFtoOktaGroup.ps1
+++ OktaScripts/AddSFtoOktaGroup.ps1
@@ -1,7 +1,9 @@
-﻿$baseUrl = "https://clearchoice.okta.com"
-$groupID = Read-Host "Enter the Okta group ID (seen in the URL when looking at the group from Okta Admin portal)"
-$authorizationToken = Read-Host "Enter the your Auth Token (generated in okta admin portal)"
-$endpoint = "/api/v1/apps/0oa1i7jjgxixhCEXP0h8/groups/" + "$groupID"
+#This interactive powershell script uses a PUT request to assign an Okta application integaration to an Okta group
+ 
+$baseUrl = "https://your_domain.okta.com" #Enter your Okta org's base URL
+$groupID = Read-Host "Enter the Okta group ID (seen in the URL when looking at the group from Okta Admin portal)" #you need to provide the ID of the Okta group
+$authorizationToken = Read-Host "Enter the your Auth Token (generated in okta admin portal)" #available to generate under the API section of the Okta Admin portal
+$endpoint = "/api/v1/apps/<APPLICATION ID>/groups/" + "$groupID" #This endpoint references the application ID of the app you'd like to assign to the group. Can be obtained from the URL of the page when viewing the app.
 $headers = @{
     "Authorization" = "SSWS $authorizationToken"
     "Content-Type" = "application/json"
@@ -12,13 +14,9 @@ try {
     $response = Invoke-RestMethod -Uri $url -Method Put -Headers $headers -ContentType "application/json"
 
     $response | out-host
-    Write-Host "Salesforce added to group!"
+    Write-Host "Application added to group!"
 }
 catch {
     Read-Host "An error occurred: $($_.Exception.Message).`nPress Enter to Exit"
     Throw $_
 }
-
-Read-Host "Press Enter to exit"
-
-#00dDZwwaUfPDUgj7WeK-yxP3sroZZxPbZeuCraAor0

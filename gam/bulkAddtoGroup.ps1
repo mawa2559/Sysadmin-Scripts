3b37ae39301f52ea65e1c1bc4028a464f5e0137f
Mark J. Walls <48637841+mawa2559@users.noreply.github.com>
2024-12-10 13:47:45 -0700

    Update bulkAddtoGroup.ps1

--- gam/bulkAddtoGroup.ps1
+++ gam/bulkAddtoGroup.ps1
@@ -1,5 +1,7 @@
-﻿Import-Csv -Path "C:\Users\mwalls\Downloads\PECs (2).csv"| ForEach-Object {
-    $email=$_."Email address"
+#A two part script to create a list of groups
+gam config csv_output_row_filter "whoCanJoin:text=ALL_IN_DOMAIN_CAN_JOIN" print groups emailmatchpattern not '<Text>.+' settings fields whoCanJoin todrive
+ Import-Csv -Path "C:\Users\mwalls\Downloads\PECs (2).csv"| ForEach-Object {
+    $email=$_."email"
     $group = "security.weavikextension.forcedallowed@clearchoice.com"
     gam update group $group add member $email
 }

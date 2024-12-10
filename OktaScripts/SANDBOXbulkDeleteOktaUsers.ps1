95bd4dc25f1f18060c4b3650b1ccc27035a95fdd
Mark J. Walls <48637841+mawa2559@users.noreply.github.com>
2024-12-10 15:27:00 -0700

    Update bulkDeleteOktaUsers.ps1

--- OktaScripts/SANDBOXbulkDeleteOktaUsers.ps1
+++ OktaScripts/SANDBOXbulkDeleteOktaUsers.ps1
@@ -1,9 +1,14 @@
-﻿
-Import-Csv -Path "C:\Users\mwalls\Downloads\Untitled spreadsheet - sandboxUsers.csv" | ForEach-Object {
-    $baseUrl = "https://clearchoice.oktapreview.com/api/v1/users/"
-    $userlogin = $_.Username
-    $authorizationToken = "000GcxqPftis_9pB_gW8NDanemou4WwPLCcvB2YPFj"
-    $deactivateEndpoint = $userlogin + "/lifecycle/deactivate"
+#This script takes a CSV containing an export of Okta usernames and deletes them all. Okta does not natively provide a bulk delete in their UI
+#This is useful in sandbox environments for removing bulk test data
+#this action is IRREVERSIBLE so be sure you want to delete these accounts
+#The easiest way to export datas is by using the rockstar Okta browser extension. Linked in the README
+
+ 
+Import-Csv -Path "C:\Path\To\Users.csv" | ForEach-Object {
+    $baseUrl = "https://yourdomain.okta.com/api/v1/users/"
+    $userlogin = $_.Username #the CSV imported has all of the users to be deleted in a column with the header "Username"
+    $authorizationToken = "API AUTH TOKEN HERE"
+    $deactivateEndpoint = $userlogin + "/lifecycle/deactivate" #Okta user must first be deactivated before they can be deleted, so both delete and deactivate endpoints are called
     $deleteEndpoint = $userlogin
     $headers = @{
         "Authorization" = "SSWS $authorizationToken"
@@ -13,8 +18,8 @@ Import-Csv -Path "C:\Users\mwalls\Downloads\Untitled spreadsheet - sandboxUsers.
 try {
     $deactivateURL = $baseUrl + $deactivateEndpoint
     $deleteURL = $baseUrl + $deleteEndpoint
-    Invoke-RestMethod -Uri $deactivateURL -Method POST -Headers $headers -ContentType "application/json" -ErrorAction Continue
-    Invoke-RestMethod -Uri $deleteURL -Method DELETE -Headers $headers -ContentType "application/json" -ErrorAction Continue
+    Invoke-RestMethod -Uri $deactivateURL -Method POST -Headers $headers -ContentType "application/json" -ErrorAction Continue #deactivate the account
+    Invoke-RestMethod -Uri $deleteURL -Method DELETE -Headers $headers -ContentType "application/json" -ErrorAction Continue #delete the account
 }
 catch {
     Write-Host "An error occurred: $($_.Exception.Message)"

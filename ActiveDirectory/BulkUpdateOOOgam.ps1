076a764d6fc50d2ed3f9d954715eaa970b617ffc
Mark J. Walls <48637841+mawa2559@users.noreply.github.com>
2024-12-10 11:18:48 -0700

    Delete ActiveDirectory/BulkUpdateOOOgam.ps1

--- ActiveDirectory/BulkUpdateOOOgam.ps1
+++
@@ -1,5 +0,0 @@
-﻿$emails = import-csv "C:\Users\mwalls\Downloads\MioEmails.csv"
-
-foreach ($user in $emails) {
-$email = $user.primaryEmail
-    gam user $email vacation on subject "Warning: You've sent an email to the wrong account!" message "This account ($email) is only for chat interoperability between Google Chat and Teams. Please search for this person in Workday to find their primary email address and use that address for all future communications." startdate 2024-11-11}

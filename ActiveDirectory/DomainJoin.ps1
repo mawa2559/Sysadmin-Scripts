616133c2b26cad5ec69602728ce52d689fdf783a
Mark J. Walls <48637841+mawa2559@users.noreply.github.com>
2024-12-10 11:21:01 -0700

    Update DomainJoin.ps1

--- ActiveDirectory/DomainJoin.ps1
+++ ActiveDirectory/DomainJoin.ps1
@@ -1,5 +1,7 @@
-﻿$domain = "ccd.local"
-$password = "" | ConvertTo-SecureString -asPlainText -Force
-$username = "$domain\mwalls-a" 
+# A simple script for joining a windows machine to the domain. Reboot should be performed afterwards.
+ 
+$domain = "domain.local" #Enter your comapny's domain
+$password = "ENTER_PASSWORD_HERE" | ConvertTo-SecureString -asPlainText -Force #Enter the domain admin's password here
+$username = "$domain\domain-Admin" #Enter the Domain Admin's username here
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
 Add-Computer -DomainName $domain -Credential $credential

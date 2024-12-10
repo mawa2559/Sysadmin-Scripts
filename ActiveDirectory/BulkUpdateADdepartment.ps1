153e40106e7603604bb1aa191fe7c4faa676ae63
Mark J. Walls <48637841+mawa2559@users.noreply.github.com>
2024-12-10 11:17:54 -0700

    Update BulkUpdateADdepartment.ps1

--- ActiveDirectory/BulkUpdateADdepartment.ps1
+++ ActiveDirectory/BulkUpdateADdepartment.ps1
@@ -1,19 +1,22 @@
-﻿$departmentUsers = Read-Host "Enter the current department attribute from AD"
+#This interactive script obtains a list of AD user who all have a given value set for Department
+#then, it iterates over that list of users and sets Department to a new value
+ 
+$departmentUsers = Read-Host "Enter the current department attribute from AD"
 $newDepartmentValue = Read-Host "Enter the new Department value (this will be applied to ALL users with the old department value)"
 try {
-    $userList = Get-ADGroupMember -Identity Ultipro | Select sAMAccountName
+    $userList = Get-ADUser -Filter "Department -eq '$departmentUsers'" | Select sAMAccountName #obtain list of users who have the specified department value
     $userList | out-host
 }
 catch {
     Read-host "Error occurred: $($_.Exception.Message).`nPress enter to exit"
     Throw $_
 }
-Read-Host "The list above contains all users with the Department attribute $departmentUsers. Hit enter to replace it with $newDepartmentValue"
+Read-Host "The list above contains all users with the Department attribute $departmentUsers. Hit enter to replace it with $newDepartmentValue" #Provide an opportunity to review the user list and the future Department value before executing change
 try {
     foreach ($user in $userList){
-         set-aduser -identity $user.sAMAccountName -department $newDepartmentValue
+         set-aduser -identity $user.sAMAccountName -department $newDepartmentValue #update the Department value
         }
-    Write-host "Department Attribute Updated Successfully!"
+    Write-host "Department Attribute Updated Successfully!" #output a success message for each user
 }
 catch {
     Read-host "Error occurred: $($_.Exception.Message).`nPress enter to exit"

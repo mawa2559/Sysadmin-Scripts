dd58938d5d68a964bd97031fa6608eb44ef613a4
Mark J. Walls <48637841+mawa2559@users.noreply.github.com>
2024-12-10 12:03:02 -0700

    Update BulkGetGroupMembers.ps1

--- gam/BulkGetGroupMembers.ps1
+++ gam/BulkGetGroupMembers.ps1
@@ -1,4 +1,7 @@
-﻿import-csv -Path "C:\Users\mwalls\Downloads\ccdGroupList.csv" | ForEach-Object {
+#Powershell script that invokes GAM
+#References a CSV containing a list of Google groups, iterates over each group and prints the associated members, exports to a csv file
+ 
+ import-csv -Path "C:\Path\To\GroupList.csv" | ForEach-Object {
     $groupname = $_."Google Group IDs"
-    gam print group-members group $groupname fields group,email | Out-File -FilePath "C:\Users\mwalls\Downloads\groupmembers.csv" -Append
+    gam print group-members group $groupname fields group,email | Out-File -FilePath "C:\Path\To\Export\groupmembers.csv" -Append
     }

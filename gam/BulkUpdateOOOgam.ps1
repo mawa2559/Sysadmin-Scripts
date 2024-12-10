2f0500c06994286c4d3d85c1210b4f76aa3a49bd
Mark J. Walls <48637841+mawa2559@users.noreply.github.com>
2024-12-10 12:59:45 -0700

    Update BulkUpdateOOOgam.ps1

--- gam/BulkUpdateOOOgam.ps1
+++ gam/BulkUpdateOOOgam.ps1
@@ -1,5 +1,10 @@
-﻿$emails = import-csv "C:\Users\mwalls\Downloads\MioEmails.csv"
+#PowerShell script that invokes GAM
+#This script takes a CSV containing a list of emails and sets their gmail Vacation Responder to automatically reply with a default message beginning on the referenced date
+#The message is customize for each user to display their email in the auto reply, and further customization using variables is simple
+ 
+$emails = import-csv "C:\Path\To\Emails.csv"
 
 foreach ($user in $emails) {
-$email = $user.primaryEmail
-    gam user $email vacation on subject "Warning: You've sent an email to the wrong account!" message "This account ($email) is only for chat interoperability between Google Chat and Teams. Please search for this person in Workday to find their primary email address and use that address for all future communications." startdate 2024-11-11}
+    $email = $user.primaryEmail #references a column in the csv with the header "primaryEmail"
+    gam user $email vacation on subject "Out of Office" message "This account ($email) is no longer active. Please email user@company.com for assistance." startdate 2024-12-10
+    }

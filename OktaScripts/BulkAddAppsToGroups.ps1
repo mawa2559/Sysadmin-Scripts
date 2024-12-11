71eb5729ff4e77782c321aa235c71b9593b1820f
Mark J. Walls <48637841+mawa2559@users.noreply.github.com>
2024-12-10 19:22:11 -0700

    Update BulkAddAppsToGroups.ps1

--- OktaScripts/BulkAddAppsToGroups.ps1
+++ OktaScripts/BulkAddAppsToGroups.ps1
@@ -1,14 +1,18 @@
-﻿$baseUrl = "https://clearchoice.okta.com"
-$authorizationToken = "00R294J0IxFMRtzL7Q3lWeaaoj5qLY-WQ1iLyQzTM5"
+#This PowerShell script takes a CSV containing an appID column and an AppLabel column mapped to a GroupID column, and adds the application to each mapped group
+#You can statically assign one app to the groups in the sheet, or reference more apps listed on the sheet. Lots of ways to customize this one
+#rockstar extension used for exporting app and group info. See README for link
+
+$baseUrl = "https://your_domain.okta.com"
+$authorizationToken = "API AUTH TOKEN HERE"
 $headers = @{
     "Authorization" = "SSWS $authorizationToken"
     "Content-Type" = "application/json"
 }
 
-Import-Csv -Path "C:\Users\mwalls\Downloads\Nextgen - update smile book apps_center groups - Smile Drive CSV.csv" | ForEach-Object {
+Import-Csv -Path "C:\Path\To\file.csv" | ForEach-Object {
 
-    $groupID = $_.GroupID
-    $appID = "0oa1xep2jbw4EKCUI0h8"  #set this $_.appID to reference the appID column of the CSV
+    $groupID = $_.GroupID #this could be set to a static group ID to assign multiple apps referenced on the CSV to a single group
+    $appID = "<APP ID>"  #set this to $_.appID (or whatever you name the appID column) to reference the appID column of the CSV to assign different apps to different groups
     $endpoint = "/api/v1/apps/" + "$appID" + "/groups/" + "$groupID"
 
     try {
@@ -17,7 +21,7 @@ Import-Csv -Path "C:\Users\mwalls\Downloads\Nextgen - update smile book apps_cen
 
         $response | out-host
         Write-Host "$_.AppLabel added to group: $_.GroupName"
-        Start-Sleep -Seconds 2 #okta rate limits the API endpiont; sleeping keeps function from failing due to 429 error
+        Start-Sleep -Seconds 2 #Okta rate limits the API endpiont; sleeping keeps function from failing due to 429 error if your CSV is referencing a lot of groups/apps
     }
     catch {
         Read-Host "An error occurred: $($_.Exception.Message).`nPress Enter to Exit"
@@ -25,5 +29,3 @@ Import-Csv -Path "C:\Users\mwalls\Downloads\Nextgen - update smile book apps_cen
     }
 
 }
-
-#00R294J0IxFMRtzL7Q3lWeaaoj5qLY-WQ1iLyQzTM5

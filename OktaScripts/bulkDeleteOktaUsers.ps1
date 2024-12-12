4fed0920a8c28bf14c96ec8fd30ee226c70cfa7e
Mark J. Walls <48637841+mawa2559@users.noreply.github.com>
2024-12-12 11:41:25 -0700

    Update bulkDeleteOktaUsers.ps1

--- OktaScripts/bulkDeleteOktaUsers.ps1
+++ OktaScripts/bulkDeleteOktaUsers.ps1
@@ -5,7 +5,7 @@
 
  
 Import-Csv -Path "C:\Path\To\Users.csv" | ForEach-Object {
-    $baseUrl = "https://yourdomain.okta.com/api/v1/users/"
+    $baseUrl = "https://yourdomain.okta.com/api/v1/users/" #update to your okta tenant url
     $userlogin = $_.Username #the CSV imported has all of the users to be deleted in a column with the header "Username"
     $authorizationToken = "API AUTH TOKEN HERE"
     $deactivateEndpoint = $userlogin + "/lifecycle/deactivate" #Okta user must first be deactivated before they can be deleted, so both delete and deactivate endpoints are called
@@ -18,8 +18,9 @@ Import-Csv -Path "C:\Path\To\Users.csv" | ForEach-Object {
 try {
     $deactivateURL = $baseUrl + $deactivateEndpoint
     $deleteURL = $baseUrl + $deleteEndpoint
-    Invoke-RestMethod -Uri $deactivateURL -Method POST -Headers $headers -ContentType "application/json" -ErrorAction Continue #deactivate the account
-    Invoke-RestMethod -Uri $deleteURL -Method DELETE -Headers $headers -ContentType "application/json" -ErrorAction Continue #delete the account
+    Invoke-RestMethod -Uri $deactivateURL -Method POST -Headers $headers -ContentType "application/json" -ErrorAction Continue | out-null #deactivating the account is required first
+    Invoke-RestMethod -Uri $deleteURL -Method DELETE -Headers $headers -ContentType "application/json" -ErrorAction Continue | out-null #then delete the account
+    write-host "$userlogin deleted"
 }
 catch {
     Write-Host "An error occurred: $($_.Exception.Message)"

e0c3d13ad15b4676e575d835f30769b20c368176
Mark J. Walls <48637841+mawa2559@users.noreply.github.com>
2024-12-14 12:25:05 -0700

    Update BulkReplace2Extensionatt.ps1

--- ActiveDirectory/BulkReplace2Extensionatt.ps1
+++ ActiveDirectory/BulkReplace2Extensionatt.ps1
@@ -1,4 +1,4 @@
-#This script use a CSV with a list of AD office values mapped to two other values
+s#This script use a CSV with a list of AD office values mapped to two other values
 #The script iterates through the CSV, generating a list of users associated with each Office value
 #Then, the script updates the two extension attributes associated with the users who have that Office value
 
@@ -6,23 +6,23 @@ $csvFilePath = "C:\Path\To\file.csv"
 
 $csvData = Import-Csv -Path $csvFilePath | ForEach-Object { #import CSV and iterate over each row
         try {
-            $OfficeLocation=$_.office #set the AD Office value to filter by
-            $props = @{"extensionAttribute1"=$_.Region
-                       "extensionAttribute2"=$_.Code} #Set the desired extension variables and save to an object
+            $OfficeLocation=$_.office #set the AD Office value to filter AD users by
+            $props = @{"extensionAttribute1"=$_.Region #get/store the value of "Region" from the csv
+                       "extensionAttribute2"=$_.Code} #get/store the value of "Code" from the csv
             $userList = Get-ADUser -Filter "Office -eq '$OfficeLocation'" | Select sAMAccountName #generate a list of users who all have the current AD Office value
             foreach ($user in $userList){ #iterate over the user list found above
                 Set-ADUser -Identity $user.sAmAccountname -replace $props #update each user
                 Write-Host "Extension Attributes Updated for user: $user.samAccountname" #output a success message for each user updated
                 }
         }
-        catch {Write-Host "Error occurred for user $user $($_.Exception.Message)"
+        catch {Write-Host "Error occurred for user $user $($_.Exception.Message)" #output any error
             }
 
     }
 
-<#example of plaintext csv valueto use with this code:
+<#example of plaintext csv data to use with this code:
 
 Office,Region,Code
 Brooklyn,Northeast,BROO
-
+Miami,Southeast,MIAM
 #>

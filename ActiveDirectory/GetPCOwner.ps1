76e301b2feb70b689fce7c5b646eb9f81028529e
Mark J. Walls <48637841+mawa2559@users.noreply.github.com>
2024-12-14 12:30:34 -0700

    Update GetPCOwner.ps1

--- ActiveDirectory/GetPCOwner.ps1
+++ ActiveDirectory/GetPCOwner.ps1
@@ -1,3 +1,10 @@
 #A simple one-liner to get the computer account owner for an AD Computer object
  
 (Get-ADComputer "PC_Name" -Properties NTSecurityDescriptor).NTSecurityDescriptor.owner #Replace the value in quotes with the computer hostname
+
+
+<# Example output:
+
+DOMAIN\admin1
+
+#>

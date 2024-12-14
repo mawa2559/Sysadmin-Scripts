52bf560ed8df9e0642e268b92f156e6b70e92c11
Mark J. Walls <48637841+mawa2559@users.noreply.github.com>
2024-12-14 12:28:41 -0700

    Update DomainJoin.ps1

--- ActiveDirectory/DomainJoin.ps1
+++ ActiveDirectory/DomainJoin.ps1
@@ -1,7 +1,7 @@
-# A simple script for joining a windows machine to the domain. Reboot should be performed afterwards.
+#A simple script for joining a windows machine to the domain. Reboot should be performed afterwards.
  
-$domain = "domain.local" #Enter your comapny's domain
-$password = "ENTER_PASSWORD_HERE" | ConvertTo-SecureString -asPlainText -Force #Enter the domain admin's password here
-$username = "$domain\domain-Admin" #Enter the Domain Admin's username here
+$domain = "domain.local" #Enter your AD domain
+$password = "ENTER_PASSWORD_HERE" | ConvertTo-SecureString -asPlainText -Force #Enter a domain admin's password here (Account must have domnain join permission)
+$username = "$domain\domain-Admin" #Enter a Domain Admin's username here
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
 Add-Computer -DomainName $domain -Credential $credential

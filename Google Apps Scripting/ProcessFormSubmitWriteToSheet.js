89e64ddb14a6450391b93d2a5bbb2abe835fa7c0
Mark J. Walls <48637841+mawa2559@users.noreply.github.com>
2025-09-10 16:11:00 -0500

    Update ProcessFormSubmitWriteToSheet.js

--- Google Apps Scripting/ProcessFormSubmitWriteToSheet.js
+++ Google Apps Scripting/ProcessFormSubmitWriteToSheet.js
@@ -1,19 +1,18 @@
 //This script takes Form data, and adds it to a Google Sheet on each submission
-//Triggers are tied to the two functions below - the triggers are owned by a service account and can only be edited by that account
-//Execution logs can be viewed in the app script console - each function includes output logged to the console when successful, or if an error is encountered
+//Triggers must be configured
 //for more info on Google Apps Scripts, built in functions, apps, and triggers, refer to https://developers.google.com/apps-script
 
 //Primary function is triggered each time a requester submits the form
 function formSubmission(e) {
   try{
-    Utilities.sleep(200); //sleep to allow form data time to process, otherwise there can be retrieval issues
+    Utilities.sleep(200); //sleep to allow form data time to process to avoid retrieval issues
     var formResponse = e.response; //get all form data from the submission
     const itemResponses = formResponse.getItemResponses(); //store each form item in an array
     var submitterEmail = formResponse.getRespondentEmail();
     var object_id = itemResponses[0].getResponse(); //get the response to the first form item in the array
-    var ss = SpreadsheetApp.openById("<spreadsheet ID>"); //saving destination spreadsheet as variable by referencing it's ID
-    var sheet = itemResponses[1].getResponse(); //saving the sheet selected by the requester on the form as a variable, 2nd form item
-    var object_list = ss.getSheetByName(sheet); //get the sheet matching the selection by requester and save as variable
+    var ss = SpreadsheetApp.openById("<spreadsheet ID>"); //save destination spreadsheet as variable by referencing it's ID
+    var sheet = itemResponses[1].getResponse(); //save the 2nd form item to a variable
+    var object_list = ss.getSheetByName(sheet); //get a sheet with a name matching the selection by requester and save as variable
     var lastRowObjectID = object_list.getLastRow(); //get the last row with data from the desired sheet
       //function writeToSheet() below writes the object id and date to the next empty row in the corresponding sheet
       function writeToSheet1() {
